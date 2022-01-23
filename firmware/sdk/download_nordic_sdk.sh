c1a3c8cb02cfe9a82710424a65adede9d880c664
Roman Turkin <roma.turkin@gmail.com>
2022-01-23 20:30:04 +0100

    [nrf] added secure bootloader

--- firmware/sdk/download_nordic_sdk.sh
+++ firmware/sdk/download_nordic_sdk.sh
@@ -9,3 +9,16 @@ unzip $FILE_NAME -d ./
 
 rm $FILE_NAME
 
+# Step 2: install uECC (for the bootloader)
+
+cd "${FOLDER_NAME}/external/micro-ecc/micro-ecc"
+
+git clone https://github.com/kmackay/micro-ecc
+
+cd nrf52hf_armgcc/armgcc
+
+make
+
+# If at this stage you get an error - you have to go to the Makefile.posix file specified in the error and 
+# update armgcc paths for your system
+
---
+++ firmware/src/targets/bootloader/README.md
@@ -0,0 +1,5 @@
+# Dictofun bootloader
+
+Secure DFU bootloader has been taken from 17.0.2 SDK with minimal modifications. 
+
+All licenses are preserved. I intent to avoid any changes in this code. 
