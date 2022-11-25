e062f3337882ba571f879f5e1b38d83cb30617ca
Roman Turkin <roma.turkin@gmail.com>
2022-11-25 11:48:24 +0100

    [android] move android out of repo

--- android/app/libs/Android-BLE-Library/examples/ble-gatt-server/src/main/java/no/nordicsemi/android/ble/ble_gatt_server/DeviceAPI.kt
+++
@@ -1,9 +0,0 @@
-package no.nordicsemi.android.ble.ble_gatt_server
-
-
-interface DeviceAPI {
-	/**
-	 * Change the value of the GATT characteristic that we're publishing
-	 */
-	fun setMyCharacteristicValue(value: String)
-}
