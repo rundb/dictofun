7344d425bdf715815fa9351b4fd6bb11ad1aeaa1
Roman Turkin <rundb@users.noreply.github.com>
2022-12-04 17:26:46 +0100

    fixed comments in the SDK download script

--- firmware/sdk/download_nordic_sdk.sh
+++ firmware/sdk/download_nordic_sdk.sh
@@ -3,22 +3,22 @@
 FOLDER_NAME="nrf5_sdk_17.1.0_ddde560"
 FILE_NAME="$FOLDER_NAME.zip"
 
-#wget "https://www.nordicsemi.com/-/media/Software-and-other-downloads/SDKs/nRF5/Binaries/${FILE_NAME}"
 wget "https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/sdks/nrf5/binaries/${FILE_NAME}"
 
 unzip $FILE_NAME -d ./
 
 rm $FILE_NAME
 
-# Step 2: install uECC (for the bootloader)
 
-cd "${FOLDER_NAME}/external/micro-ecc/micro-ecc"
+# # Step 2: install uECC (for the bootloader, uncomment when this functionality is enabled)
 
-git clone https://github.com/kmackay/micro-ecc
+# cd "${FOLDER_NAME}/external/micro-ecc/micro-ecc"
 
-cd nrf52hf_armgcc/armgcc
+# git clone https://github.com/kmackay/micro-ecc
 
-make
+# cd nrf52hf_armgcc/armgcc
+
+# make
 
 # If at this stage you get an error - you have to go to the Makefile.posix file specified in the error and 
 # update armgcc paths for your system
