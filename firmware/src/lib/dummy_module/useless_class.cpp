7fc74e0defe4e01d96d7a2211715540b92e93246
Roman <rundb@users.noreply.github.com>
2022-12-07 14:35:46 +0100

    [ut] added support of gtest and a test example (#71)
    * [ut] added support of gtest and a test example (+CI config)

---
+++ firmware/src/lib/dummy_module/useless_class.cpp
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier:  Apache-2.0
+/*
+ * Copyright (c) 2022, Roman Turkin
+ */
+#include "useless_class.h"
+
+namespace useless
+{
+
+int UselessClass::add(int a, int b)
+{
+    return a + b;
+}
+
+}
