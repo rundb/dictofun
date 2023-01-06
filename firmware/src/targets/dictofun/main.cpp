59f904517ea86c97d81cdd2c66e18a9290588c37
Roman Turkin <rundb@users.noreply.github.com>
2023-01-06 14:59:32 +0100

    refactoring: move freertos wrappers out of main.cpp

--- firmware/src/targets/dictofun/main.cpp
+++ firmware/src/targets/dictofun/main.cpp
@@ -22,65 +22,19 @@
 
 #include <stdint.h>
 
+#include "freertos_wrappers.hpp"
+
 // clang-format off
 // ============================= Tasks ======================================
-template<size_t stack_size_, UBaseType_t priority_>
-struct TaskDescriptor
-{
-    static constexpr size_t stack_size{stack_size_};
-    static constexpr UBaseType_t priority{priority_};
-    StackType_t stack[stack_size] {0UL};
-    StaticTask_t task;
-    TaskHandle_t handle{nullptr};
 
-    result::Result init(TaskFunction_t function, const char * task_name, void * task_parameters)
-    {
-        handle = xTaskCreateStatic(
-            function,
-            task_name,
-            stack_size,
-            task_parameters,
-            priority,
-            stack,
-            &task);
-        if (nullptr == handle)
-        {
-            return result::Result::ERROR_GENERAL;
-        }
- 
-        return result::Result::OK;
-    }
-};
-
-TaskDescriptor<256, 1> audio_task;
-TaskDescriptor<256, 1> log_task;
-TaskDescriptor<256, 2> systemstate_task;
+application::TaskDescriptor<256, 1> audio_task;
+application::TaskDescriptor<256, 1> log_task;
+application::TaskDescriptor<256, 2> systemstate_task;
 
 // ============================= Queues =====================================
-template<typename T, size_t N>
-struct QueueDescriptor
-{
-    using type = T;
-    static constexpr size_t element_size{sizeof(T)};
-    static constexpr size_t depth{N};
 
-    StaticQueue_t queue;
-    uint8_t buffer[N * sizeof(T)];
-    QueueHandle_t handle{nullptr};
-
-    result::Result init()
-    {
-        handle = xQueueCreateStatic( depth, element_size, buffer, &queue);
-        if (nullptr == handle)
-        {
-            return result::Result::ERROR_GENERAL;
-        }
-        return result::Result::OK;
-    }
-};
-
-QueueDescriptor<logger::CliCommandQueueElement, 1> cli_commands_queue;
-QueueDescriptor<logger::CliStatusQueueElement, 1> cli_status_queue;
+application::QueueDescriptor<logger::CliCommandQueueElement, 1> cli_commands_queue;
+application::QueueDescriptor<logger::CliStatusQueueElement, 1> cli_status_queue;
 
 // ============================ Contexts ====================================
 logger::CliContext      cli_context;
