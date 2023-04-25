6e0dc6b72724f7a7b9ba4ac74f2a5b4793353bb7
Roman Turkin <rundb@users.noreply.github.com>
2023-04-25 15:08:38 +0200

    [ci] add setup script for rpi ci

---
+++ firmware/ci/setup_rpi_ci.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+wget https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/desktop-software/nrf-command-line-tools/sw/versions-10-x-x/10-21-0/nrf-command-line-tools_10.21.0_arm64.deb
+
+sudo apt install ./nrf-command-line-tools_10.21.0_arm64.deb
+
+sudo apt install /opt/nrf-command-line-tools/share/JLink_Linux_V780c_arm64.deb --fix-broken
+
+wget https://github.com/NordicSemiconductor/nrf-udev/releases/download/v1.0.1/nrf-udev_1.0.1-all.deb
+
+sudo dpkg -i nrf-udev_1.0.1-all.deb
+
+sudo usermod -a -G dialout "$(whoami)"
+
+rm -f "nrf-command-line-tools_10.21.0_arm64.deb"
+rm -f "nrf-udev_1.0.1-all.deb"
+
+echo "A re-login is required after execution of the script"
