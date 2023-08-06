6e2fc98ec0e0519f5bcac537cb05783b630c3f23
Roman Turkin <rundb@users.noreply.github.com>
2023-08-06 18:00:59 +0200

    [build] improved version file generation by using cmake built in functionality

--- firmware/src/targets/dictofun/version/version.py
+++ firmware/src/targets/dictofun/version/version.py
@@ -1,5 +1,8 @@
 import os
 import platform
+import sys
+
+# First argument passed into this script is the binary path, retrieved from CMake
 
 git_show_output = os.popen("git show -s --format=\"%h %ci\" HEAD").read()[:-1]
 git_branch_output = os.popen("git rev-parse --abbrev-ref HEAD").read()[:-1]
@@ -7,7 +10,9 @@ hostname = platform.node()
 
 build_info_data = "\"Dictofun FW. " + git_show_output + ". " + "branch: " + git_branch_output + ". Built at: " + hostname + "\""
 
-with open("../../../../../firmware/src/targets/dictofun/version/version.cpp", "w") as f:
+current_build_path = sys.argv[1]
+
+with open(current_build_path + "/version.cpp", "w+") as f:
     f.write("#include \"version.h\"\n")
     f.write("namespace version\n{\n")
     f.write("const char BUILD_SUMMARY_STRING[] = \n    " + build_info_data + ";\n")
