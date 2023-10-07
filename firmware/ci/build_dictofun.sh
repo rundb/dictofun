7d1b4085747332d7c5e8d604c23a56fdc44ff277
Roman Turkin <rundb@users.noreply.github.com>
2023-10-07 22:04:39 +0200

    apply patches in the Docker CI image

--- firmware/ci/build_dictofun.sh
+++ firmware/ci/build_dictofun.sh
@@ -7,6 +7,14 @@ fi
 
 rm -rf ./*
 
+# Apply patches on the SDK, if not done so yet
+patch "/sdk/nRF5_SDK_17.1.0_ddde560/components/softdevice/common/nrf_sdh_freertos.c" < "../sdk/sdh_stack_size_increase.patch" \
+  && patch "/sdk/nRF5_SDK_17.1.0_ddde560/components/softdevice/common/nrf_sdh_freertos.c" < "../sdk/nrf_sdh_pause_resume_c.patch" \
+  && patch "/sdk/nRF5_SDK_17.1.0_ddde560/components/softdevice/common/nrf_sdh_freertos.h" < "../sdk/nrf_sdh_pause_resume_h.patch" \
+  && patch "/sdk/nRF5_SDK_17.1.0_ddde560/components/ble/ble_services/ble_cts_c/ble_cts_c.c" < "../sdk/nrf_cts_c_reduce_log.patch" \
+  && patch "/sdk/nRF5_SDK_17.1.0_ddde560/components/ble/nrf_ble_gq/nrf_ble_gq.c" < "../sdk/nrf_gq_reduce_logs.patch"
+
+
 cmake -S .. -B . \
     -DCMAKE_BUILD_TYPE:STRING=Debug \
     -DBUILD_TARGET:STRING=dictofun \
